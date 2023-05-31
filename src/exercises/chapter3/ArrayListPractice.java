f7458501b26c86ea1f90bdf456d056f9e3bd9140
Kavitha <kavithamsca@gmail.com>
2023-05-31 13:06:50 -0500

    Modified the code in ArrayList

--- src/exercises/chapter3/ArrayListPractice.java
+++ src/exercises/chapter3/ArrayListPractice.java
@@ -8,20 +8,20 @@ package exercises.chapter3;
 
 public class ArrayListPractice {
     public static void main(String[] args) {
-//        ArrayList<Integer> nums = new ArrayList<>();
-//        nums.add(2);
-//        nums.add(4);
-//        nums.add(6);
-//        nums.add(5);
-//        nums.add(9);
-//        nums.add(13);
-//        nums.add(15);
-//        nums.add(17);
-//        nums.add(22);
-//        nums.add(25);
-//        System.out.println("ArrayList is:"+ nums);
-//        int sum =sumEvenNumbers(nums);
-//        System.out.println(sum);
+        ArrayList<Integer> nums = new ArrayList<>();
+        nums.add(2);
+        nums.add(4);
+        nums.add(6);
+        nums.add(5);
+        nums.add(9);
+        nums.add(13);
+        nums.add(15);
+        nums.add(17);
+        nums.add(22);
+        nums.add(25);
+        System.out.println("ArrayList is:"+ nums);
+        int sum =sumEvenNumbers(nums);
+        System.out.println(sum);
         ArrayList<String> words = new ArrayList<>();
         words.add("dogs");
         words.add("monkey");
