1363f998e8e1fc1ac483a861334efcd9b23e8ed2
Kavitha <kavithamsca@gmail.com>
2023-05-31 20:26:51 -0500

    completed Bonus

--- src/org/launchcode/java/studios/countingcharacters/CountingCharacters.java
+++ src/org/launchcode/java/studios/countingcharacters/CountingCharacters.java
@@ -2,16 +2,28 @@ package org.launchcode.java.studios.countingcharacters;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Scanner;
 
 public class CountingCharacters {
     public static void main(String[] args){
-        String Quote = "If the product of two terms is zero then common sense says at least one of the two terms has to be zero to start with." + "" +
-                " So if you move all the terms over to one side, you can put the quadratics into a form that can be factored allowing that side of the equation to equal zero." + "" +
-                " Once you’ve done that, it’s pretty straightforward from there." ;
+//        String Quote = "If the product of two terms is zero then common sense says at least one of the two terms has to be zero to start with." +
+//                " So if you move all the terms over to one side, you can put the quadratics into a form that can be factored allowing that side of the equation to equal zero. " +
+//                "Once you’ve done that, it’s pretty straightforward from there." ;
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter a phrase, and i'll count each letter in your phrase:");
+        String Quote = input.nextLine().toLowerCase();
+
+
             char[] charactersInString = Quote.toCharArray();
         HashMap<Character , Integer> QuoteStr = new HashMap<>();
+         for (char str : charactersInString){
+             if (!Character.isAlphabetic(str)){
+                 continue;
+             }
 
-        for(char str : charactersInString ){
+
+
+       //for(char str : charactersInString){
             if(QuoteStr.containsKey(str)){
                 QuoteStr.put(str,QuoteStr.get(str)+1);
             }
@@ -19,8 +31,8 @@ public class CountingCharacters {
                 QuoteStr.put(str,1);
             }
         }
-       for(Map.Entry Character : QuoteStr.entrySet()){
-           System.out.println(Character.getKey() + " : " + Character.getValue());
+       for(Map.Entry<Character, Integer> Characters : QuoteStr.entrySet()){
+           System.out.println(Characters.getKey() + ": " + Characters.getValue());
        }
     }
 
